d1d0013a0dec0f370b3a568179aa6142b66f4d4e
Rauha Rahkola <rrahkola@gmail.com>
2024-11-27 13:04:13 -0800

    archiving 2023 days

---
+++ dec12-passage_pathing/README.md
@@ -0,0 +1,3 @@
+# [--- Day 1: Trebuchet ---](https://adventofcode.com/2023/day/1)
+
+## [--- Part Two ---](https://adventofcode.com/2023/day/1#part2)
---
+++ dec12-passage_pathing/index.js
@@ -0,0 +1,34 @@
+import { strict as assert } from 'assert'
+import { inspect } from 'util'
+
+/*  Given <output of interpret>,
+ *  yields
+ */
+function* part1(data, config) {
+  const { showIntermediate } = config
+  yield 'Howdy'
+}
+
+/*  Given an array of input: ['', ...],
+ *  yields
+ */
+function interpret(input) { return input }
+
+export default function* pickPart(input, config) {
+  let result
+  assert(
+    Array.isArray(input) && input.length > 0,
+    'Must provide data as array of strings, use options "-t lines"'
+  )
+  assert([1, 2].includes(config.part), 'Valid parts are 1 or 2')
+
+  const data = interpret(input)
+  if (config.showIntermediate) yield inspect(data, { depth: null, maxArrayLength: null })
+  if (config.part === 1) {
+    // Find answer for part 1
+    for (const output of part1(data, config)) yield inspect(output), result = output
+  } else {
+    // Find answer for part 2
+    for (const output of part1(data, config)) yield inspect(output), result = output
+  }
+}
