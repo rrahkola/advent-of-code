9e2d5ea0ef45aedab669585383a7481bc636a385
Rauha Rahkola <rrahkola@gmail.com>
2024-12-02 21:54:48 -0800

    fixing template README.md; finished dec03; minor fixes

--- dec02-reports/README.md
+++ dec02-reports/README.md
@@ -1,4 +1,4 @@
-# [--- Day 1: Red-Nosed Reports ---](https://adventofcode.com/2024/day/2)
+# [--- Day 2: Red-Nosed Reports ---](https://adventofcode.com/2024/day/2)
 Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
 
 While the [Red-Nosed Reindeer nuclear fusion/fission plant](https://adventofcode.com/2015/day/19) appears to contain no sign of the Chief Historian, the engineers there run up to you as soon as they see you. Apparently, they **still** talk about the time Rudolph was saved through molecular synthesis from a single electron.
---
+++ dec03-mull_it_over/index.js
@@ -0,0 +1,50 @@
+import { strict as assert } from 'assert'
+import { inspect } from 'util'
+
+/*  Given <output of interpret>,
+ *  yields
+ */
+function* multiplyPairs(matches, config) {
+  const { showIntermediate, ignoreToggles = true } = config
+  const result = matches.reduce((acc, { a, b, c = true }) => {
+    if (showIntermediate) console.log({ ignoreToggles, a, b, c })
+    return (ignoreToggles || c) ? acc + a * b : acc
+  }, 0)
+  yield result
+}
+
+/*  Given an array of input: ['<some_text>mul(123,4)<more_text>', ...],
+ *  identifies all the `mul(a,b)` expressions and returns matching results.
+ *  Also keeps track of `do()` and `don't()` instructions to enable/disable the `mul` operations.
+ */
+function interpret(input) {
+  const instructionRegex = /mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)|(?<enable>do\(\))|(?<disable>don't\(\))/g
+  const matches = []
+  let match, c = true
+  for (const line of input) {
+    while( (match = instructionRegex.exec(line)) !== null) {
+      const { a, b, enable, disable } = match.groups
+      if (enable) c = true
+      if (disable) c = false
+      if (a && b) matches.push({ a: Number.parseInt(a), b: Number.parseInt(b), c })
+    }
+  }
+  return matches
+}
+
+export default function* pickPart(input, config) {
+  let result
+  assert(
+    Array.isArray(input) && input.length > 0,
+    'Must provide data as array of strings, use options "-t lines"'
+  )
+  assert([1, 2].includes(config.part), 'Valid parts are 1 or 2')
+
+  const data = interpret(input)
+  if (config.showIntermediate) yield inspect(data, { depth: null, maxArrayLength: null })
+  if (config.part === 2) {
+    // Find answer for part 2
+    config.ignoreToggles = false
+  }
+  for (const output of multiplyPairs(data, config)) yield inspect(output), result = output
+}
